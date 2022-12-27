139c56d99f61e869cd0dc32f17126d189d84e209
Aruka <yshida1pc7@gmail.com>
2022-12-27 18:04:10 +0900

    update : データ保存の部分をすっきりさせました

--- src/main/java/com/github/zauworks/chatdictionary/util/DataSave.java
+++ src/main/java/com/github/zauworks/chatdictionary/util/DataSave.java
@@ -15,39 +15,18 @@ public class DataSave {
 
     public void main(String type, CommandSender sender){
         if(type.equalsIgnoreCase("all")){
-            saveAll();
+            for(Map.Entry<String,DictionaryWord> entry:ChatDictionary.getDictionaryWords().entrySet()){
+                save(entry.getKey());
+            }
         }else if (!ChatDictionary.getDictionaryWords().containsKey(type)){
             sender.sendMessage(new Messages().error("[Error]:Config save error. Not contains the value."));
         }else{
-            saveSelected(type);
+            save(type);
         }
         sender.sendMessage(new Messages().success("[Save]:Complete to save the configuration."));
     }
 
-    private void saveAll(){
-        Map<String,DictionaryWord> words = ChatDictionary.getDictionaryWords();
-        for(Map.Entry<String, DictionaryWord> entry:words.entrySet()){
-            DictionaryWord dict = entry.getValue();
-            String word = entry.getKey();
-            String mean = dict.getMean();
-            String color = dict.getColorCode();
-            boolean bold = dict.isBold();
-            boolean underline = dict.isUnderline();
-            String savePath = " dictionary."+word+".";
-
-            config.set(savePath+"color",color);
-            config.set(savePath+"bold",bold);
-            config.set(savePath+"underline",underline);
-            if(mean.contains("\n")){
-                config.set(savePath+"mean", Arrays.asList(mean.split("\n")));
-            }else{
-                config.set(savePath+"mean",mean);
-            }
-             instance.saveConfig();
-        }
-    }
-
-    private void saveSelected(String word){
+    private void save(String word){
         DictionaryWord dict = ChatDictionary.getDictionaryWords().get(word);
         String mean = dict.getMean();
         String color = dict.getColorCode();
